af727cee20627920371d1bdeaf2d03226a47bf73
unknown <3159729324@qq.com>
2023-03-23 19:02:31 +0800

    version_1.0.3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
-import App from './App.vue'
+
+import App from './App'
+import store from './store'
+import router from './router'
+
+//引入饿了吗ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
+//引入相关API请求接口
+import API from '@/api'
+//注册全局组件
+// import CategorySelect from '@/components/CategorySelect';
+// Vue.component(CategorySelect.name,CategorySelect);
+
+//挂载到Vue原型上，任意组件都可以使用API接口
+Vue.prototype.$API = API;
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
