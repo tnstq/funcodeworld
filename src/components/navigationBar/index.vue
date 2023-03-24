3937b8fd664d4ae2490140f2d8a20c9cb74164f6
zh <1594428906@qq.com>
2023-03-24 10:22:00 +0800

    version_1.0.4

---
+++ src/components/navigationBar/index.vue
@@ -0,0 +1,245 @@
+<template>
+  <div>
+    <!--导航栏 -->
+    <!-- 导航栏 -->
+    <nav>
+      <div class="nav-swaper">
+        <li class="nav-swaper-lists">
+          <a href="index.html">
+            <img src="@/assets/logo.png" />
+          </a>
+        </li>
+        <li>
+          <a href="index.html">
+            <span class="nav-swaper-listscolor"> <b>首页</b></span>
+          </a>
+        </li>
+        <li>
+          <a href="index.html">
+            <span class="nav-swaper-listscolor"> <b>作品</b> </span>
+          </a>
+        </li>
+        <li>
+          <a href="index.html">
+            <span class="nav-swaper-listscolor"> <b>论坛</b></span>
+          </a>
+        </li>
+        <li>
+          <a href="index.html">
+            <span class="nav-swaper-listscolor"> <b>创作</b> </span>
+          </a>
+        </li>
+        <!-- 搜索框 -->
+        <li class="nav-swaper-input">
+          <div class="nav-swaper-input-outer">
+            <div class="dropdown">
+              <button class="dropbtn" id="dropbtn">作品</button>
+              <input class="dropInp" id="dropInp" type="text" />
+              <button class="dropserach">
+                <img src="@/assets/logo.png" alt="" />
+              </button>
+
+              <div class="dropdown-content">
+                <a
+                  href="#"
+                  target="_blank"
+                  class="dropdown-contentone"
+                  id="dropdown-contentone"
+                  >作品</a
+                >
+                <a href="#" class="dropdown-contentone" id="dropdown-contenttwo"
+                  >论文</a
+                >
+              </div>
+            </div>
+          </div>
+        </li>
+            <!-- ************************ 登录注册页面跳转*************************** -->
+<!-- 
+        <li >
+          <span class="nav-swaper-listscolor"
+            ><a href="" target="_blank">登录</a></span
+          >
+          <span class="nav-swaper-listscolor"> | </span>
+          <span class="nav-swaper-listscolor"
+            ><a href="" target="_blank">注册</a></span
+          >
+        </li> -->
+        <!-- 用来显示登录成功后的样式 -->
+        <li class="userCenter">
+            <div class="userCenter-avatar">
+                <img src="" alt="">
+            </div>
+            <div class="showUser">
+                <li>帖子</li>
+                <li>个人信息</li>
+                <li>退出</li>
+            </div>
+        </li>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  // 导航栏
+  name: "NavigationBar",
+};
+</script>
+
+<style lang="less" scoped>
+
+.nav-swaper {
+  font-size: 16px;
+  margin-top: 20px;
+  margin-left: 10px;
+  margin-right: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .nav-swaper-lists {
+    a {
+      img {
+        display: inline-block;
+        width: 200px;
+        height: 60px;
+      }
+    }
+  }
+
+//  用户登录成功后头像显示
+.userCenter{
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    border: 1px solid gray;
+    // 图片的大小设置
+    .userCenter-avatar{
+
+    }
+
+    // 展示操作信息
+    .showUser{
+        li{
+            display: inline-block;
+            width: 40px;
+            height: 30px;
+            background: gray;
+            border: 1px solid snow;
+        }
+    }
+}
+  .nav-swaper-listscolor:hover {
+    a {
+      color: orange;
+      border-bottom: 2px solid orange;
+    }
+    b {
+      color: orange;
+      border-bottom: 2px solid orange;
+    }
+  }
+
+  // 下拉菜单
+  .nav-swaper-input {
+    .nav-swaper-input-outer:hover {
+      border: 1px solid rgb(17, 12, 3);
+    }
+    .nav-swaper-input-outer {
+      width: 180px;
+      height: 30px;
+      border: 1px solid orange;
+      border-radius: 50px;
+      .dropbtn {
+        background-color: rgba(0, 0, 0, 0);
+        color: black;
+        // padding: 16px;
+        margin-left: 10px;
+        margin: 4px 10px;
+        font-size: 10px;
+        font-weight: bold;
+        border: none;
+        cursor: pointer;
+      }
+
+      /* 容器 <div> - 需要放置下拉内容 */
+      .dropdown {
+        position: relative;
+        display: inline-block;
+        .dropInp {
+          position: absolute;
+          top: 4px;
+          left: 50px;
+          // border:1px solid #2185D0;
+          height: 20px;
+          width: 70px;
+        }
+        .dropserach {
+          position: absolute;
+          top: 5px;
+          left: 120px;
+          // border:1px solid #2185D0;
+          background-color: white;
+          border-radius: 20px;
+          height: 20px;
+          width: 50px;
+          img {
+            width: 20px;
+            height: 18px;
+          }
+        }
+        .dropserach:hover {
+          // border:1px solid #2185D0;
+          background-color: rgba(255, 165, 0, 0.4);
+        }
+      }
+
+      /* 下拉内容（默认隐藏） */
+      .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: #f9f9f9;
+        min-width: 70px;
+        font-size: 12px;
+        font-weight: bold;
+        box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+        z-index: 1;
+      }
+
+      /* 下拉链接 */
+      .dropdown-content a {
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+      }
+
+      /* 悬停时更改下拉链接的颜色 */
+      .dropdown-content a:hover {
+        background-color: #f1f1f1;
+      }
+
+      /* 悬停时显示下拉菜单  .dropdown:hover .dropdown-content */
+      //   .dropbtn:hover ~.dropdown-content {
+      //     display: block;
+      //     // color: red;
+      //   }
+      .dropdown:hover .dropdown-content {
+        margin: 0;
+        display: block;
+      }
+
+      .dropInp:hover ~ .dropdown-content {
+        margin: 0;
+        display: none;
+      }
+
+      .dropserach:hover ~ .dropdown-content {
+        margin: 0;
+        display: none;
+      }
+    }
+  }
+}
+</style>
